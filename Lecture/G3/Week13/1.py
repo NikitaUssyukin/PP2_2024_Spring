ff0540b03b99c4ef3a1a548cfba604900b8098e8
NikitaUssyukin <nikus0108@gmail.com>
2024-04-18 14:30:55 +0500

    update

---
+++ Lecture/G3/Week13/1.py
@@ -0,0 +1,27 @@
+# Databases
+
+# Lets imagine a database with students
+# Such a database will contain:
+# - name
+# - ID
+# - study year
+# - phone number
+
+# Lets take a spreadsheet as an example of a database
+# Table represents one class of entities (e.g. students)
+# Rows in the table represent instances of this class, i.e.
+# individual entities (students). Rows are also called records or tuples
+# Columns represent attributes related to these entities
+
+# RDB - Relational Database
+# Relational databases store data also in tables. 
+
+# RDBMS - Relational Database Management System
+
+# CRUD - Create, Read, Update, Delete
+# CRUD describes 4 main operations with the database
+
+# PostgreSQL (also known as Postgres) will be used as our RDBMS
+
+
+
